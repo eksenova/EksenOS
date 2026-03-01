e93cbb805f1cb188ca2c0b37d040c562a5befd48
Enes Sadık Özbek <1809172+Trojaner@users.noreply.github.com>
2026-03-01 15:28:28 +0300

    Add ApplyQueryFilter extension

---
+++ packages/Eksen.EntityFrameworkCore/src/ModelBuilderExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq.Expressions;
+using Eksen.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace Eksen.EntityFrameworkCore;
+
+public static class ModelBuilderExtensions
+{
+    public static void ApplyQueryFilter<TBaseEntity>(
+        this ModelBuilder builder,
+        Expression<Func<TBaseEntity, bool>> filter)
+    {
+        var acceptableItems = builder.Model.GetEntityTypes()
+            .Where(et => typeof(TBaseEntity).IsAssignableFrom(et.ClrType))
+            .ToList();
+
+        foreach (var entityType in acceptableItems)
+        {
+            var entityParam = Expression.Parameter(entityType.ClrType, name: "e");
+
+            var filterBody = ReplacingExpressionVisitor.Replace(filter.Parameters[index: 0], entityParam, filter.Body);
+            var filterLambda = entityType.GetDeclaredQueryFilters().FirstOrDefault(f => f.IsAnonymous)?.Expression;
+         
+            if (filterLambda != null)
+            {
+                filterBody = ReplacingExpressionVisitor.Replace(entityParam, filterLambda.Parameters[index: 0], filterBody);
+                filterBody = Expression.AndAlso(filterLambda.Body, filterBody);
+                filterLambda = Expression.Lambda(filterBody, filterLambda.Parameters);
+            }
+            else
+            {
+                filterLambda = Expression.Lambda(filterBody, entityParam);
+            }
+
+            entityType.SetQueryFilter(filterLambda);
+        }
+    }
+
+    public static void ApplyEksenQueryFilters<TBaseEntity>(
+        this ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplySoftDeleteQueryFilter<TBaseEntity>();
+    }
+
+    public static void ApplySoftDeleteQueryFilter<TBaseEntity>(
+        this ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyQueryFilter<ISoftDelete>(e => !e.IsDeleted);
+    }
+}
